f5bcbaaca7dfcebf96ec1cfe92d82f3f4a05252c
Kierian <hey@kierian.me>
2023-12-12 18:23:40 +0700

    fix: alias import

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 import "./src/env.mjs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/*": path.resolve(__dirname, "src"),
+    };
+
+    return config;
+  },
+};
 
 export default config;
