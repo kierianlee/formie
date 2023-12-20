6aa7ee8e2308610238cdcf32e210a1a944f2f896
Kierian <hey@kierian.me>
2023-12-20 17:50:01 +0700

    feat: posthog setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,11 @@ import Header from "./_components/header";
 import Footer from "./_components/footer";
 import { Toaster } from "react-hot-toast";
 import NextTopLoader from "nextjs-toploader";
+import { Suspense } from "react";
+import { PHProvider, PostHogPageview } from "./posthog";
 
 import "@/app/globals.css";
+import { env } from "@/env.mjs";
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -39,6 +42,7 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     startupImage: ["https://formie.dev/logo-small.png"],
   },
+  metadataBase: new URL(env.NEXT_PUBLIC_BASE_URL),
 };
 
 export const viewport: Viewport = {
@@ -55,29 +59,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "flex min-h-screen flex-col bg-background font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
-        <NextTopLoader color="hsl(131 38% 74%)" />
-        <NextAuthProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            disableTransitionOnChange
-          >
-            <Header />
-            <main className="flex flex-1 flex-col">{children}</main>
-            <Footer />
+      <Suspense>
+        <PostHogPageview />
+      </Suspense>
+      <PHProvider>
+        <body
+          className={cn(
+            "flex min-h-screen flex-col bg-background font-sans antialiased",
+            fontSans.variable,
+          )}
+        >
+          <NextTopLoader color="hsl(131 38% 74%)" />
+          <NextAuthProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              disableTransitionOnChange
+            >
+              <Header />
+              <main className="flex flex-1 flex-col">{children}</main>
+              <Footer />
 
-            <Toaster
-              toastOptions={{ className: "text-sm !bg-zinc-800 !text-white" }}
-            />
-          </ThemeProvider>
-        </NextAuthProvider>
-      </body>
+              <Toaster
+                toastOptions={{ className: "text-sm !bg-zinc-800 !text-white" }}
+              />
+            </ThemeProvider>
+          </NextAuthProvider>
+        </body>
+      </PHProvider>
     </html>
   );
 }
