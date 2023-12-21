f84680edcf22071f0201d3c6c93f35e6b758a25f
Kierian <hey@kierian.me>
2023-12-21 10:35:35 +0700

    fix: wrong code snippets

--- src/app/_data/code-snippets.ts
+++ src/app/_data/code-snippets.ts
@@ -5,25 +5,25 @@ export const htmlSnippet = `<form action="https://formie.dev/{form_id}" method="
 </form>`;
 
 export const reactSnippet = `<form action="https://formie.dev/{form_id}" method="post">
-  <input name="Email" id="email" type="email">
-  <textarea name="Message" id="message">
+  <input name="Email" id="email" type="email" />
+  <textarea name="Message" id="message" />
   <button type="submit">Submit</button>
 </form>`;
 
 export const svelteSnippet = `<form action="https://formie.dev/{form_id}" method="post">
-  <input name="Email" id="email" type="email">
-  <textarea name="Message" id="message">
+  <input name="Email" id="email" type="email" />
+  <textarea name="Message" id="message" />
   <button type="submit">Submit</button>
 </form>`;
 
 export const vueSnippet = `<form action="https://formie.dev/{form_id}" method="post">
-  <input name="Email" id="email" type="email">
-  <textarea name="Message" id="message">
+  <input name="Email" id="email" type="email" />
+  <textarea name="Message" id="message" />
   <button type="submit">Submit</button>
 </form>`;
 
 export const angularSnippet = `<form ngNoForm action="https://formie.dev/{form_id}" method="post">
-  <input name="Email" id="email" type="email">
-  <textarea name="Message" id="message">
+  <input name="Email" id="email" type="email" />
+  <textarea name="Message" id="message" />
   <button type="submit">Submit</button>
 </form>`;
