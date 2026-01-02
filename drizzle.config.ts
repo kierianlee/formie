25eb3d420fd528f51418ef7044cef35f973b4985
Kierian <hey@kierian.me>
2026-01-02 14:34:08 +0700

    chore: upgrade drizzle kit version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,8 +8,8 @@ if (!process.env.DATABASE_URL)
 export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 } satisfies Config;
